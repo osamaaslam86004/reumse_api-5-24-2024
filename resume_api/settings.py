9113d204487a2a5405139415124832e8bbef0050
osama aslam <144129880+osamaaslam86004@users.noreply.github.com>
2024-02-27 01:11:39 +0500

    Your commit message here

--- resume_api/settings.py
+++ resume_api/settings.py
@@ -88,11 +88,17 @@ WSGI_APPLICATION = "resume_api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "neondb",
+        "USER": config("PGUSER")
+        "PASSWORD": config("PGPASSWORD"),
+        "HOST":  config("HOST"),
+        "PORT": "5432",
+        "OPTIONS": {
+            "sslmode": "require",
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
