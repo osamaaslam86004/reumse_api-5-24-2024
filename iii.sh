16796a8f71253b1ff1539e31a1063d2c90f7891f
osama aslam <144129880+osamaaslam86004@users.noreply.github.com>
2024-02-27 18:58:03 +0500

    pip

--- iii.sh
+++ iii.sh
@@ -1,5 +1,5 @@
- echo "BUILD START"
- python -m pip install -r requirements.txt
- python manage.py makemigrations --noinput
- python manage.py migrate --noinput
- echo "BUILD END"
+echo "BUILD START"
+python3.11 -m pip install -r requirements.txt
+python3.11 manage.py makemigrations --noinput
+python3.11 manage.py migrate --noinput
+echo "BUILD END"
