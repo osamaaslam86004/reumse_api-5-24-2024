c65bb4bd5ad3ceb655ed43a4a1ad926115dd0a3b
osama aslam <144129880+osamaaslam86004@users.noreply.github.com>
2024-02-27 20:38:28 +0500

    iii.sh

--- iii.sh
+++ iii.sh
@@ -1,5 +1,5 @@
 echo "BUILD START"
-python3.11 -m pip install --no-cache-dir --disable-pip-version-check -r requirements.txt
+python3.9 -m pip install --no-cache-dir --disable-pip-version-check -r requirements.txt
 # python3.11 manage.py makemigrations --noinput
 # python3.11 manage.py migrate --noinput
 echo "BUILD END"
