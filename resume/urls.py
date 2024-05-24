4389c2af6fb46eed73928eb401a96564cfedceac
osamaaslam86004 <osama.aslam.86004@gmail.com>
2024-05-24 13:23:57 +0500

    edited resume.urls

--- resume/urls.py
+++ resume/urls.py
@@ -9,18 +9,21 @@ from resume.views import (
     ProgrammingAreaViewSet,
     ProjectsViewSet,
     PersonalInfoWizard,
-    PersonalInfoView,
+    # PersonalInfoView,
     PersonalInfo_List_CreateView,
-    PersonalInfoPreviewWizard
+    # PersonalInfoPreviewWizard
 )
 from resume.forms import PersonalInfoForm
 from rest_framework.routers import DefaultRouter
 
 
-
 router = DefaultRouter()
 router.register(r"personal-info-data", PersonalInfoViewSet)
-router.register(r"get-personal-info-data", PersonalInfo_List_CreateView, basename='get-personal-info-data')
+router.register(
+    r"get-personal-info-data",
+    PersonalInfo_List_CreateView,
+    basename="get-personal-info-data",
+)
 router.register(r"overview", OverviewViewSet)
 router.register(r"education", EducationViewSet)
 router.register(r"skill", SkillViewSet)
@@ -29,35 +32,36 @@ router.register(r"projects", ProjectsViewSet)
 
 
 urlpatterns = [
-
-
-    path("get-personal-info-data-for-user/", PersonalInfo_List_CreateView.as_view({
-                                                                       'get': 'list',
-                                                                       'get' : 'get_personal_info_for_user'}),
-                                                                        name ="get_personal_info_for_user"),
-                                                                        
-    path("patch-put-personal-info-data-for-user/", PersonalInfo_List_CreateView.as_view({
-                                                                       'put': 'patch_personal_info_for_user',
-                                                                       'patch' : 'patch_personal_info_for_user'}),
-                                                                        name ="patch_put_personal_info_for_user"),
-
-                                                                        
+    path(
+        "get-personal-info-data-for-user/",
+        PersonalInfo_List_CreateView.as_view(
+            {"get": "list", "get": "get_personal_info_for_user"}
+        ),
+        name="get_personal_info_for_user",
+    ),
+    path(
+        "patch-put-personal-info-data-for-user/",
+        PersonalInfo_List_CreateView.as_view(
+            {
+                "put": "patch_personal_info_for_user",
+                "patch": "patch_personal_info_for_user",
+            }
+        ),
+        name="patch_put_personal_info_for_user",
+    ),
     path(
         "",
         PersonalInfoWizard.as_view(),
         name="personal-info-wizard",
     ),
-
-    path(
-        "preview/",
-        views.PersonalInfoPreviewWizard(PersonalInfoForm),
-        name="personal-info-preview_wizard",
-    ),
-    path(
-        "form/",
-        PersonalInfoView.as_view(),
-        name="personal-info-view",
-    ),
-
+    # path(
+    #     "preview/",
+    #     views.PersonalInfoPreviewWizard(PersonalInfoForm),
+    #     name="personal-info-preview_wizard",
+    # ),
+    # path(
+    #     "form/",
+    #     PersonalInfoView.as_view(),
+    #     name="personal-info-view",
+    # ),
 ] + [path("api/", include(router.urls))]
-
