52665fbfecd027b647a507db81096d007bef40d7
osamaaslam86004 <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>
2024-06-13 18:29:30 +0500

    signals url

--- resume/signals.py
+++ resume/signals.py
@@ -24,8 +24,12 @@ def create_profile(sender, instance, **kwargs):
     if _serializer and not getattr(_serializer, "is_create", False):
         return
 
-    logger.error(f"serializer: {_serializer}---- : is_create : {getattr(_serializer, 'is_create', False)}")
-    print(f"serializer: {_serializer}---- : is_create : {getattr(_serializer, 'is_create', False)}")
+    logger.error(
+        f"serializer: {_serializer}---- : is_create : {getattr(_serializer, 'is_create', False)}"
+    )
+    print(
+        f"serializer: {_serializer}---- : is_create : {getattr(_serializer, 'is_create', False)}"
+    )
 
     if instance:
         data = {
@@ -39,7 +43,7 @@ def create_profile(sender, instance, **kwargs):
         data = json.dumps(data)
         logger.error(f"data in signal: {data}")
 
-        if settings.DEBUG:
+        if not settings.DEBUG:
             webhook_url = "https://osama11111.pythonanywhere.com/cv-webhook/"
         else:
             webhook_url = "https://diverse-intense-whippet.ngrok-free.app/cv-webhook/"
@@ -78,7 +82,7 @@ def store_cv_attributes(sender, instance, **kwargs):
 def send_webhook_on_cv_delete(sender, instance, **kwargs):
     attributes = TemporaryStorage.storage.pop(instance.id, None)
 
-    if settings.DEBUG:
+    if not settings.DEBUG:
         webhook_url = "https://osama11111.pythonanywhere.com/cv-webhook/"
     else:
         webhook_url = "https://diverse-intense-whippet.ngrok-free.app/cv-webhook/"
